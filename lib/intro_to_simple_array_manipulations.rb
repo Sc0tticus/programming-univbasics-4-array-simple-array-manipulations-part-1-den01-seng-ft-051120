b1c57edaf4f8cd81ab9d350ae0c6e5d315c1be12
Scott Singer <ssinger303@gmail.com>
2020-04-30 19:14:15 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+
+def using_push(array,string)
+  array.push(string)
+ p array
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+ p array
+end
+
+def using_pop(array)
+element = array.pop
+p element
+end
+
+def pop_with_args(array)
